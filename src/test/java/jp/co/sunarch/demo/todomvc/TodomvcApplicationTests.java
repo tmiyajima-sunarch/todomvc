15f8b9b34c94d8c7892717bb3d385ac5ef7058fa
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2019-02-19 01:11:15 +0900

    initial commit

---
+++ src/test/java/jp/co/sunarch/demo/todomvc/TodomvcApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.sunarch.demo.todomvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodomvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
