bdf068a7e4e586d39e8e1187147334b0b3932619
tmiyajima-sunarch <47751609+tmiyajima-sunarch@users.noreply.github.com>
2022-07-02 01:46:53 +0000

    コンパイラモードを3に変更

--- todomvc-vue/vue.config.js
+++ todomvc-vue/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
           ...options,
           compilerOptions: {
             compatConfig: {
-              MODE: 2
+              MODE: 3
             },
           },
         };
