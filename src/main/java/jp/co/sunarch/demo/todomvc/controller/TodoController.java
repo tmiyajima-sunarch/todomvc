5b89c04e72e08c19a6a2841382a590640a2430d9
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2023-06-10 07:07:32 +0900

    chore: upgrade to spring-boot 3.1.0

--- src/main/java/jp/co/sunarch/demo/todomvc/controller/TodoController.java
+++ src/main/java/jp/co/sunarch/demo/todomvc/controller/TodoController.java
@@ -1,14 +1,23 @@
 package jp.co.sunarch.demo.todomvc.controller;
 
-import jp.co.sunarch.demo.todomvc.domain.Todo;
-import jp.co.sunarch.demo.todomvc.service.TodoService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 import java.security.Principal;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jp.co.sunarch.demo.todomvc.domain.Todo;
+import jp.co.sunarch.demo.todomvc.service.TodoService;
 
 @RestController
 @RequestMapping("/api/todos")
